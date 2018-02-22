67efd3dc6c324406bc6ec58faa8cf3e0d2b5fb31
Filipe Morelli <morellitecinfo@gmail.com>
2018-02-21 22:05:26 -0300

    Framework7

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,32 @@
-// Ionic Starter App
+// Dom7
+var $$ = Dom7;
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// Framework7 App main instance
+var app  = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  // App routes
+  routes: routes,
+  panel: {
+    swipe: 'left',
+  },
+});
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+  url: '/'
+});
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var username = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
---
+++ www/js/routes.js
@@ -0,0 +1,19 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+  },
+  {
+    path: '/form/',
+    url: './pages/form.html',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
