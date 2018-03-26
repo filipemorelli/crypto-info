42245299c8be11f3a3d56500ffc6767e55720fd1
Filipe Morelli <morelliteicnfo@gmail.com>
2018-03-25 23:41:55 -0300

    Base para app crypto

--- www/js/app.js
+++ www/js/app.js
@@ -1,117 +1,75 @@
 // Dom7
 var $$ = Dom7;
 
-// Framework7 App main instance
-var app = new Framework7({
-    root: '#app', // App root element
-    id: 'io.framework7.testapp', // App bundle ID
-    name: 'Framework7', // App name
-    theme: 'auto', // Automatic theme detection
-    // App routes
-    routes: routes
-});
+angular.module("app-crypto", ["provider.app", "run.app"]);
 
-// Init/Create main view
-var mainView = app.views.create('.view-main', {
-    url: '/'
-});
+angular.module("run.app", []).run([
+    function () {
 
-document.addEventListener('backbutton', function () {
-    // on device back button go back
-    if (mainView.history.length > 1) {
-        mainView.router.back();
-    } else {
-        if ($$(".dialog").length === 0) {
-            app.dialog.confirm("Deseja Realmente sair do app", "Tutors App", function name(params) {
-                navigator.app.exitApp();
-            }, null);
-        }
-    }
-}, false);
-
-document.addEventListener("deviceready", function onDeviceReady() {
-    if (window.plugins && window.plugins.AdMob) {
-        var ad_units = {
-            ios: {
-                banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx', //PUT ADMOB ADCODE HERE
-                interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx' //PUT ADMOB ADCODE HERE
-            },
-            android: {
-                banner: 'ca-app-pub-9420257838571875/3347861187', //PUT ADMOB ADCODE HERE
-                interstitial: 'ca-app-pub-9420257838571875/4331083441' //PUT ADMOB ADCODE HERE
-            }
-        };
-        var admobid = (/(android)/i.test(navigator.userAgent)) ? ad_units.android : ad_units.ios;
-
-        window.plugins.AdMob.setOptions({
-            publisherId: "ca-app-pub-9420257838571875/3347861187",
-            interstitialAdId: "ca-app-pub-9420257838571875/4331083441",
-            adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER, //use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD
-            bannerAtTop: false, // set to true, to put banner at top
-            overlap: false, // banner will overlap webview
-            offsetTopBar: false, // set to true to avoid ios7 status bar overlap
-            isTesting: false, // receiving test ad
-            autoShow: true // auto show interstitial ad when loaded
+        // Framework7 App main instance
+        var app = new Framework7({
+            root: '#app', // App root element
+            id: 'app.crypto', // App bundle ID
+            name: 'CryptoAgora', // App name
+            theme: 'auto', // Automatic theme detection
+            // App routes
+            routes: routes
         });
 
-        if (navigator.onLine) {
-            doAdMob();
-        } else {
-            var timerAdmob;
-            timerAdmob = setInterval(function () {
-                if (navigator.onLine) {
-                    doAdMob();
-                    clearInterval(timerAdmob);
+        // Init/Create main view
+        var mainView = app.views.create('.view-main', {
+            url: '/'
+        });
+
+        // In page events:
+        $$(document).on('page:init', function (e) {
+            // Page Data contains all required information about loaded and initialized page
+            var page = e.detail;
+        });
+
+        $$('.panel-right').on('panel:open', function () {
+            var range = app.range.create({
+                el: '.range-slider',
+                on: {
+                    change: function (e, values) {
+                        console.log(values);
+                    }
                 }
-            }, 10000);
-        }
+            });
+        });
 
-    } else {
-        alert('admob plugin not ready');
-    }
-
-    function doAdMob() {
-        registerAdEvents();
-        window.plugins.AdMob.createBannerView();
-        window.plugins.AdMob.createInterstitialView(); //get the interstitials ready to be shown
-        window.plugins.AdMob.requestInterstitialAd();
-    }
-
-    //functions to allow you to know when ads are shown, etc.
-    function registerAdEvents() {
-        document.addEventListener('onReceiveAd', function () {});
-        document.addEventListener('onFailedToReceiveAd', function (data) {});
-        document.addEventListener('onPresentAd', function () {});
-        document.addEventListener('onDismissAd', function () {});
-        document.addEventListener('onLeaveToAd', function () {});
-        document.addEventListener('onReceiveInterstitialAd', function () {});
-        document.addEventListener('onPresentInterstitialAd', function () {});
-        document.addEventListener('onDismissInterstitialAd', function () {
-            // window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW
-            // window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed
+        // create searchbar
+        var searchbar = app.searchbar.create({
+            el: '.searchbar',
+            searchContainer: '.list-teste',
+            searchIn: '.item-title',
+            on: {
+                search(sb, query, previousQuery) {
+                    console.log(query, previousQuery);
+                }
+            }
         });
     }
-}, false);
+]);
 
-// In page events:
-$$(document).on('page:init', function (e) {
-    // Page Data contains all required information about loaded and initialized page
-    var page = e.detail;
-    showBanner();
-    setTimeout(function () {
-        CodeMirror.colorize(document.querySelectorAll("pre"), "text/x-sh");
-        CodeMirror.colorize(document.querySelectorAll("pre[type=javascript]"), "text/javascript");
-        CodeMirror.colorize(document.querySelectorAll("pre[type=html]"), "text/html");
-    }, 200)
-});
+angular.module("provider.app", ["pascalprecht.translate"]).config([
+    "$translateProvider",
+    function ($translateProvider) {
+        $translateProvider.useStaticFilesLoader({
+            'prefix': '/translations/',
+            'suffix': '.json'
+        });
+        var lang = getLang();
+        console.log(lang);
+        $translateProvider.preferredLanguage(lang);
 
-var counterPage = 0;
-
-function showBanner() {
-    if (window.plugins && window.plugins.AdMob) {
-        counterPage++;
-        if (counterPage % 3 === 0) {
-            window.plugins.AdMob.requestInterstitialAd();
+        function getLang() {
+            switch (navigator.language) {
+                case "pt-BR":
+                    return navigator.language;
+                default:
+                    return "en-US";
+            }
         }
     }
-}
+]);
